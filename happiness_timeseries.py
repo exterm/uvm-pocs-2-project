06aede8d5161363fe50a74f647c88468a3daf87f
Philip Mueller <mail@philip.in-aachen.net>
2023-03-03 22:25:12 -0500

    shift timeseries to center windows

--- happiness_timeseries.py
+++ happiness_timeseries.py
@@ -9,6 +9,32 @@ import pandas as pd
 
 from lib import happiness
 
+def set_axes(axes, last):
+    axes.yaxis.set_major_formatter('{x:.2f}')
+    axes.set_ylabel("Happiness score")
+    # only label the x-axis on the bottom plot
+    if last:
+        axes.set_xlabel("Time (in words elapsed)")
+
+def mark_seasons(axes, season_indices, last):
+    for i, season_index in enumerate(season_indices):
+        axes.axvline(season_index, color='red', linestyle='--')
+
+    # add a caption to each season line in the last plot
+    if last:
+        for i, season_index in enumerate(season_indices):
+            axes.text(
+                season_index + 5000,
+                axes.get_ylim()[0],
+                f"Season {i + 1}",
+                verticalalignment='bottom',
+                horizontalalignment='left'
+            )
+
+def shift_timeseries(timeseries, shift):
+    return [None] * shift + timeseries
+
+
 parser = argparse.ArgumentParser(
     description='Generate a timeseries of happiness scores for a given wordlist.'
 )
@@ -37,6 +63,9 @@ window_sizes = [round(10 ** z) for z in WINDOW_EXPONENTS]
 print("read wordlist")
 wordlist_full = pd.read_csv(args.wordlist)
 
+# truncate wordlist to a reasonable size
+# wordlist_full = wordlist_full[:100000]
+
 # just the tokens
 wordlist = wordlist_full['Token'].to_list()
 
@@ -45,9 +74,6 @@ seasons = wordlist_full['Season'].to_list()
 
 max_season = max(seasons)
 season_indices = [seasons.index(season) for season in range(1, max_season + 1)]
-
-# truncate wordlist to a reasonable size
-# wordlist = wordlist[:100000]
 
 if args.profile:
     cProfile.run('happiness.timeseries(window_size, wordlist, happiness_scores)', 'restats')
@@ -63,29 +89,6 @@ fig, axes = plt.subplots(num_plots, 1, sharex=True, sharey=False)
 # increase vertical spacing between plots
 fig.subplots_adjust(hspace=0.5)
 
-def set_axes(axes, last):
-    axes.yaxis.set_major_formatter('{x:.2f}')
-    axes.set_ylabel("Happiness score")
-    # only label the x-axis on the bottom plot
-    if last:
-        axes.set_xlabel("Time (in words elapsed)")
-
-def mark_seasons(axes, season_indices, last):
-    for i, season_index in enumerate(season_indices):
-        axes.axvline(season_index, color='red', linestyle='--')
-
-    # add a caption to each season line in the last plot
-    if last:
-        for i, season_index in enumerate(season_indices):
-            axes.text(
-                season_index + 5000,
-                axes.get_ylim()[0],
-                f"Season {i + 1}",
-                verticalalignment='bottom',
-                horizontalalignment='left'
-            )
-
-
 if args.lenses:
     for i, lens in enumerate(LENSES):
         print(f"lens: {lens}")
@@ -96,6 +99,9 @@ if args.lenses:
             happiness_scores,
             lens
         )
+
+        timeseries = shift_timeseries(timeseries, PREFERRED_WINDOW_SIZE // 2)
+
         axes[i].plot(timeseries)
         axes[i].set_title(f"lens: {lens}, window size: {PREFERRED_WINDOW_SIZE}")
 
@@ -107,6 +113,9 @@ else:
         print(f"window size: {window_size}")
 
         timeseries = happiness.timeseries_fast(window_size, wordlist, happiness_scores)
+
+        timeseries = shift_timeseries(timeseries, window_size // 2)
+
         axes[i].plot(timeseries)
         axes[i].set_title(f"Window size: {window_size}")
 
