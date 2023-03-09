99bb36757e0d5f5883c25fb768a491675c22877f
Philip Mueller <mail@philip.in-aachen.net>
2023-03-09 15:02:21 -0500

    more parameters for word shifts

--- generate_wordshift.py
+++ generate_wordshift.py
@@ -14,10 +14,16 @@ parser = argparse.ArgumentParser(
 parser.add_argument('reference', type=str, help='Reference CSV file')
 parser.add_argument('comparison', type=str, help='Comparison CSV file')
 
-# optional argument: which column to use
 parser.add_argument('--column', type=str, default="Token", help='Which column to use')
+parser.add_argument('--baseline', type=str, default='average', help='Which value to use as neutral; defaults to the average of the reference system.')
+
+parser.add_argument('--reference-name', type=str, default="reference", help='Name to use for the reference system')
+parser.add_argument('--comparison-name', type=str, default="comparison", help='Name to use for the comparison system')
 
 args = parser.parse_args()
+
+if args.baseline != 'average':
+    args.baseline = float(args.baseline)
 
 reference = pd.read_csv(args.reference)[args.column]
 comparison = pd.read_csv(args.comparison)[args.column]
@@ -34,6 +40,6 @@ if args.column == "Token":
 else:
     scores = {t: 1 for t in reference_bag.keys() | comparison_bag.keys()}
 
-shift = sh.shifterator.Shift(reference_bag, comparison_bag, type2score_1=scores, type2score_2=scores, stop_lens=[(3,7)])
+shift = sh.shifterator.Shift(reference_bag, comparison_bag, type2score_1=scores, type2score_2=scores, stop_lens=[(3,7)], reference_value=args.baseline)
 
-shift.get_shift_graph(system_names=["reference", "comparison"])
+shift.get_shift_graph(system_names=[args.reference_name, args.comparison_name])
