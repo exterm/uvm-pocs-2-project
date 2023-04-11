f1458a4d1146b11f7960bd1d1c92208f155b8fb4
Philip Mueller <mail@philip.in-aachen.net>
2023-04-11 16:46:29 -0400

    allow generating wordshifts for different types of scores

--- generate_wordshift.py
+++ generate_wordshift.py
@@ -4,6 +4,7 @@ import argparse
 from collections import Counter
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 import shifterator as sh
 
@@ -20,6 +21,8 @@ parser.add_argument('--baseline', type=str, default='average', help='Which value
 parser.add_argument('--reference-name', type=str, default="reference", help='Name to use for the reference system')
 parser.add_argument('--comparison-name', type=str, default="comparison", help='Name to use for the comparison system')
 
+parser.add_argument('--score-type', type=str, default="labMT", help='Which score type to use')
+
 args = parser.parse_args()
 
 if args.baseline != 'average':
@@ -31,15 +34,25 @@ comparison = pd.read_csv(args.comparison)[args.column]
 reference_bag = Counter(reference)
 comparison_bag = Counter(comparison)
 
+stop_lens = None
+
 if args.column == "Token":
-    # print("read happiness scores")
-    # scores = pd.read_csv('Hedonometer.csv', usecols=['Word', 'Happiness Score'])
-    # scores = scores.set_index('Word')
-    # scores = scores.to_dict()['Happiness Score']
-    scores = 'labMT_English'
+    if args.score_type == 'labMT':
+        scores = 'labMT_English'
+        stop_lens = [(3,7)]
+    else:
+        print(f"read {args.score_type} scores")
+        scores = pd.read_csv('ousiometry-data/ousiometry_data_augmented.tsv', usecols=['Word', args.score_type], sep='\t')
+        scores = scores.set_index('Word')
+        scores = scores.to_dict()[args.score_type]
 else:
     scores = {t: 1 for t in reference_bag.keys() | comparison_bag.keys()}
 
-shift = sh.shifterator.Shift(reference_bag, comparison_bag, type2score_1=scores, type2score_2=scores, stop_lens=[(3,7)], reference_value=args.baseline)
+shift = sh.shifterator.Shift(reference_bag, comparison_bag, type2score_1=scores, type2score_2=scores, stop_lens=stop_lens, reference_value=args.baseline)
 
-shift.get_shift_graph(system_names=[args.reference_name, args.comparison_name])
+ax = shift.get_shift_graph(system_names=[args.reference_name, args.comparison_name], show_plot=False)
+
+# create more space for title of plot
+plt.subplots_adjust(top=0.9)
+
+plt.show()
